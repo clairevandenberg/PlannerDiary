f584168454f77f804847f466f0ea11ec90aff7c6
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-03-19 20:19:28 +1030

    Time and date is now displayed!

--- script.js
+++ script.js
@@ -9,25 +9,11 @@
 //var CurrentDate = document.querySelector ("m");
 
 
-// Current Date + Time 
-//var m = moment().format('MMMM Do YYYY, h:mm:ss a')
-//console.log(moment);
+//Current Date + Time 
+var currentDate = moment().format('MMMM Do YYYY, h:mm a')
+console.log(currentDate);
+//Display Current Date + Time on Scheduler 
 
-//var m = moment('2013-03-01', 'YYYY-MM-DD')
-//console.log(m)
+var eDisplayMoment = document.getElementById('currentDate');
+eDisplayMoment.innerHTML = currentDate;
 
-//moment.utc('2016-01-01T00:00:00+02:00').format()
-//"2015-12-31T22:00:00+00:00"
-
-//function isDate(string) {
-  //  {
-       // moment(string).format(["MM-DD-YYYY", "YYYY-MM-DD"]);
-   // }
-//}
-//console.log(moment)
-
-const today = moment();
-console.log(
-  "Today's date is: " + 
-  today.format('YYYY-MM-DD')
-);
