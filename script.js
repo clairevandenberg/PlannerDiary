e16d603833fdb817060e5156c32ed06217983ab0
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-03-24 12:34:33 +1030

    fixing loop

--- script.js
+++ script.js
@@ -1,141 +1,79 @@
-//var future = document.querySelector("future");
-//var hover = document.querySelector("hover");
-//var present = document.querySelector("present");
-//var past = document.querySelector("past");
-//var hour = document.querySelector("hour");
-//var currentDay = document.querySelector ("currentDay");
-//var lead = document.querySelector ("lead");
-//var CurrentDate = document.querySelector ("m");
-
 var plannertimes = document.querySelector ("plannertimes");
 var containerTime = document.querySelector("time-block-container");
 //var currentTime = document.querySelector("#hour");
 var time = document.querySelector("#time-block");
-//Current Date + Time 
+
+//Current Date  
 var currentDate = moment().format('dddd, MMMM Do YYYY, h:mm a')
 console.log(currentDate);
 
-
 // Create & Display Time
 // For Loop for times of the day 
+ 
+
+for ( let i = 8; i <19; i++) {
+let newRow = $("<div class= 'time-block heightSpace row'>");
+if(i <=12)
 { 
-var time = "";
-var i;
-for ( i = 8; i <25; i++) {
-time += "" + i + "<br>";
+var time= $("<div class= 'hour col-md-2'>").text(i +"am");
+}else {
+time = $("<div class= 'hour col-md-2'>").text(i - 12 +"pm");
+   }
 }
- document.getElementById("timeHour").innerHTML = time;
+console.log(time)
+//time += "" + i + "<br>";
 
-//current time starting colum of time
-var currentTime = moment().startOf ('hh');
-console.log(currentTime);
+ //document.getElementById("timeHour").innerHTML = time;
 
-//.isSame
-moment('2010-10-20').isSame('2009-12-31', 'year'); 
+//  // object for moment to calculate isBefore and isSame
+// var date;
+// var Currenthour;
+//  let momenthour = date.hour(i);
 
-//.isBefore
-moment('2010-10-20').isBefore('2010-10-21'); //
+// //current time starting colum of time
+// var currentTime = moment().startOf ('hh');
+// console.log(currentTime);
 
-//.isPresent
-?
+// //.isSame
 
+// //.isBefore
+// let descriptionClass = momenthour.isBefore(moment(), 'hour')
 
-//putting the current time into a row
-let hour = document.createElement("div");
-hour.classList.add("hour", "col-2");
-hour.innerText = currentTime.format("hh A");
-}
+// //moment(time).isBefore(time); //
 
-// Create Display Content 
-let content = document.createElement("div");
-content.classList.add("content", "col-8");
-//allow element to be editable so user can type their to dos
-content.setAttribute("contenteditable", true);
+// //.isPresent
+// // ?
 
 
+// //putting the current time into a row
+// let hour = document.createElement("div");
+// hour.classList.add("hour", "col-2");
+// hour.innerText = currentTime.format("hh A");
+// }
 
-// Create and Display Save Button 
-let saveBtn = document.createElement("div");
-saveBtn.classList.add("saveBtn", "col-2");
-saveBtn.setAttribute("time" ("i"));
-saveBtn.addEventListener("click", dataSaved);
+// // Create Display Content 
+// let content = document.createElement("div");
+// content.classList.add("content", "col-8");
+// //allow element to be editable so user can type their to dos
+// content.setAttribute("contenteditable", true);
 
-//end time 
-//var endTime = moment().endOf('day')
-//console.log(endTime);
+// // Create and Display Save Button 
+// let saveBtn = document.createElement("div");
+// saveBtn.classList.add("saveBtn", "col-2");
+// saveBtn.setAttribute("time" ("i"));
+// saveBtn.addEventListener("click", dataSaved);
 
-//Display Current Date + Time on Scheduler 
-//var eDisplayMoment = document.getElementById('currentDate');
-//eDisplayMoment.innerHTML = currentDate;
+// // setting stype
 
-//var dateOuterDiv = document.createElement("div");
-   // dateOuterDiv.classList.add("time-block");
+// // hour 
+// ("hour", "col-2");
+// // descripton 
+// ("content", "col-8");
+// // save 
+// ("saveBtn", "col-2");
 
-//let hourRow = document.createElement("div");
-   // hourRow.classList.add("row");
+// // current time 
+// ("present")
 
-// Diplaying Time 
-// times of the day \
-//function time(i) {
- //   if (i < 23) {
-  //      i = "0" + i;
- //     }
- //     return i;
- //   }
-//function timeContainer () {
-//var date= new Date();
-//var demo = document.getElementById("demo");
-//var hours = time(d.getHours());
-//x.innerHTML = hours
-//}
-
-
-
-// Displaying Content 
-
-// Displaying 
-
-
-
-
-
-
-
-
-
-//var plannertimes= {
-   // "0900" : '1',
-   // '1000' : '2',
-  //  '1100' : '3',
-  //  '1200' : '4',
-  //  '1300' : '5',
-  //  '1400' : '6',
- //   '1500' : '7',
-//   '1600' : '8',
-  //  '1700' : '9'
-//}
-//console.log(plannertimes);
-
-//Sorting Planner Numbers
-//{let plannertimes = [2 , 3, 4, 5, 6, 7, 8, 9, 1 ];
-//plannertimes.sort((a,b) => a - b);
-//console.log(plannertimes);
-//}
-
-
-
-// This .on("click") function will planner times
-//$("plannertimes").on("click", function (event) {
- // event.preventDefault()
- // )};
-
-// This .on("click") function will toDo
-//$("toDo").on("click", function (event) {
-//    event.preventDefault()
-//)};
-
-//var today = moment 
-//var yesturday = moment 
-//today.format ()
-//yesturday.fomrat ()
-//yesturday.isBefore (t)
+// //future
+// ("future");
