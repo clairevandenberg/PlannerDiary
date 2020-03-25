f248da0c178eaa0ffc56932eb82a4371f3e87e23
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-03-25 23:41:32 +1030

    debugging

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ var toDo = [];
 
 for (let i = 8; i <19; i++) {
 let index = i - 8;
+console.log(`i = ${i}`);
 let newRow = $("<div class= 'time-block row'>");
 if (i <=12)
 { 
@@ -26,29 +27,37 @@ var savedValue = localStorage.getItem(i);
 savedValue = "";
 
 newRow.append(time, savedValue, toDo);
+console.log(newRow)
+$(".container").append(newRow);
+console.log(`^^New Row`);
+
 }
 console.log(savedValue);
 
 
 // organising past time
-if (moment().format ("hh") > i) {
+if (moment().format ("HH") > i) {
 var time = $("<div class='col-md-8> <textarea class='description" + i + "past textInput'>" + savedValue + "</textarea></div>");
+console.log("Past");
 }
 console.log(time);
 
 
 // organising current time
-if (moment().format ('hh') === i) {
+if (moment().format ('HH') == i) {
    var time = $("<div class='col-md-8> <textarea class='description" + i + "present textInput'>" + savedValue + "</textarea></div>");
-   }
+   console.log("Current");
+}
    console.log(time);
    
 
 // organising future time 
-if (moment().format ('hh') < i) {
+if (moment().format ('HH') < i) {
    var time = $("<div class='col-md-8> <textarea class='description" + i + "future textInput'>" + savedValue + "</textarea></div>");
-   }
+   console.log("Future");
+}
    console.log(time);
+   newRow.append(time);
    }
 
 // Hour Description 
@@ -61,10 +70,9 @@ $("toDo").submit(function( event ){
 // console.log(toDo);
 
 function setStyles() {
-   var savedValue = localStorage.getItem('/saveBtn');
+   var savedValue = localStorage.getItem('.saveBtn');
    var time = localStorage.getItem('.time-block');
    var toDo = localStorage.getItem('.description');
-   newRow.append(time, savedValue, toDo);
 
 }
 // Save Button
