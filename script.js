18b785fd9115ae44263dc26148d5a3b8a0d0f458
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-03-25 22:18:46 +1030

    de bugging

--- script.js
+++ script.js
@@ -4,14 +4,13 @@ var currentTime = document.querySelector("#hour");
 
 //Current Date  
 var currentDate = moment().format('dddd, MMMM Do YYYY, h:mm a')
-$("#currentDate").text(currentTime)
 console.log(currentDate);
 
 // Create & Display Time
 // For Loop for times of the day 
 var toDo = [];
 
-for ( let i = 8; i <19; i++) {
+for (let i = 8; i <19; i++) {
 let index = i - 8;
 let newRow = $("<div class= 'time-block row'>");
 if(i <=12)
@@ -19,16 +18,16 @@ if(i <=12)
 var time= $("<div class= 'hour col-md-2'>").text(i + "am");
 }else {
 time = $("<div class= 'hour col-md-2'>").text(i - 12 + "pm");
-   }
-   console.log (time)
-   if (localStorage.getItem(i) !== null) {
-      var savedValue = localStorage.getItem(i);
-   }else {
-      savedValue = "";
+}
+console.log (time)
+if (localStorage.getItem(i) !== null) {
+var savedValue = localStorage.getItem(i);
+}else {
+savedValue = "";
 
-   $(".container").append(time, toDo);
-   }
-   console.log(savedValue);
+$(".container").append(time, toDo);
+}
+console.log(savedValue);
 
 
 // organising past time
@@ -36,6 +35,7 @@ if (moment().format ("hh") > i) {
 var time = $("<div class='col-md-8> <textarea class='description" + i + "past textInput'>" + savedValue + "</textarea></div");
 }
 console.log(time);
+
 
 // organising current time
 if (moment().format ('hh') === i) {
@@ -58,14 +58,14 @@ toDo.text(textarea[index]) = todoText
 console.log(toDo);
 
 
-
 // Save Button
-$(savedValue).on("click", function (event) {
-   event.preventDefault ();
-   let index = $(this).attr("data-btnIndex");
-   let toDo = $(".input"+index).val();
-});
-   
+$(savedValue).on("click"), function () {
+event.preventDefault ();
+var savedValue= $("<div class = 'saveBtn col-md-2>")
+   let index = $(this).attr("savedValue Index");
+   let toDo = $(".input"+ index).val();
+};
+  
 
 
 //time += "" + i + "<br>";
